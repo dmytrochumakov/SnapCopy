3fa71ad754588b96e9ba39261fcba16d9d762516
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-02-02 18:00:23 +0200

    Initial commit

---
+++ SnapCopy/SnapCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCopyApp.swift
+//  SnapCopy
+//
+//  Created by Dmytro Chumakov on 02.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnapCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
