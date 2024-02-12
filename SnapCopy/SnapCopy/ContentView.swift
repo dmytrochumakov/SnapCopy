331775d2a56783da9349b3179b5c80d31950a35f
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-02-12 16:39:21 +0200

    added: add item functionality

--- SnapCopy/SnapCopy/ContentView.swift
+++ SnapCopy/SnapCopy/ContentView.swift
@@ -5,36 +5,47 @@
 //  Created by Dmytro Chumakov on 10.02.2024.
 //
 
+import Combine
 import SwiftUI
 import TipKit
-import Combine
 
 final class ViewModel: ObservableObject {
     private var cancellables: Set<AnyCancellable> = []
     @Published private(set) var items: [Item] = []
     @Published private(set) var isButtonDisabled = !UIPasteboard.general.hasStrings
     @Published var searchQuery = ""
+    @Published var showAddItemView: Bool = false
 
     init() {
-         $searchQuery            
-            .debounce(for: .milliseconds(300), scheduler: RunLoop.main)            
-            .removeDuplicates()            
+        $searchQuery
+            .debounce(for: .milliseconds(300), scheduler: RunLoop.main)
+            .removeDuplicates()
             .sink { [weak self] query in
                 self?.performSearch(query: query)
-            }            
+            }
             .store(in: &cancellables)
     }
 
-func performSearch(query: String) {     
+    func performSearch(query: String) {
         print("Searching for: \(query)")
     }
+
     func paste() {
         if let string = UIPasteboard.general.string {
-            if !items.contains(where: { $0.name == string }) {
-                items.append(.init(name: string))
-            }
+            addIfPossible(string)
         }
     }
+
+    func addNewItem(_ newItem: String) {
+addIfPossible(newItem)
+    }
+
+    func addIfPossible(_ newItem: String)  {
+if !items.contains(where: { $0.name == newItem }) {
+                items.append(.init(name: newItem))
+            }
+    }
+
     func goToSettings() {
         guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
         UIApplication.shared.open(url)
@@ -45,9 +56,10 @@ struct Item: Identifiable, Hashable {
     var id: String { name }
     let name: String
 }
+
 struct ContentView: View {
     @StateObject private var viewModel = ViewModel()
-    
+
     init() {
         if #available(iOS 17.0, *) {
             #if DEBUG
@@ -56,45 +68,55 @@ struct ContentView: View {
             try? Tips.configure()
         }
     }
+
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
-              pasteButton
-                List(viewModel.items) {  item in
-                    TextField("Item", text: .constant(item.name))
-                }
-                .navigationTitle("SnapCopy")
-                    .searchable(text: $viewModel.searchQuery, prompt: "Search Items")
-                    .toolbar {
-                        ToolbarItem(placement: .topBarTrailing) {
-                            HStack(spacing: 0) {
-                                Button("Add") {
-
-                                }
-                            }
+                pasteButton
+                listTtemsView
+                NavigationLink(
+                    destination: AddItemView(addTapped: { newItem in 
+                        viewModel.addNewItem(newItem)
+                    }), 
+                    isActive: $viewModel.showAddItemView
+                    ) { EmptyView() }
+            }
+            .navigationTitle("SnapCopy")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 0) {
+                        Button("Add") {
+                            viewModel.showAddItemView = true
                         }
                     }
+                }
             }
         }
-
     }
+
+    private var listTtemsView: some View {
+        List(viewModel.items) { item in
+            TextField("Item", text: .constant(item.name))
+        }
+        .searchable(text: $viewModel.searchQuery, prompt: "Search Items")
+    }
+
     private var pasteButton: some View {
-     if #available(iOS 17.0, *) {
+        if #available(iOS 17.0, *) {
             return defaultPasteButton
-             .popoverTip(GoToSettingsTip()) { _ in
-                 viewModel.goToSettings()
-             }
+                .popoverTip(GoToSettingsTip()) { _ in
+                    viewModel.goToSettings()
+                }
         } else {
             return defaultPasteButton
         }
     }
 
     private var defaultPasteButton: some View {
-          Button("Paste") {
-                    viewModel.paste()
-                }
-                .disabled(viewModel.isButtonDisabled)
-
+        Button("Paste") {
+            viewModel.paste()
+        }
+        .disabled(viewModel.isButtonDisabled)
     }
 }
 
@@ -102,13 +124,33 @@ struct ContentView: View {
     ContentView()
 }
 
-
 @available(iOS 17.0, *)
 struct GoToSettingsTip: Tip {
     var id: String { Self.titleValue }
     static let titleValue: String = "To avoid annoying meesage about the clipboard access, please go to \nSettings -> SnapCopy -> Allow access to clipboard."
-    var title: Text = Text(titleValue)
-    var actions: [Action] = [.init({
+    var title: Text = .init(titleValue)
+    var actions: [Action] = [.init {
         Text("Open Settings")
-    })]
+    }]
+}
+
+struct AddItemView: View {
+    @Environment(\.dismiss) var dismiss
+
+    @State private var text: String = ""
+    private let addTapped: (String) -> Void
+    init(addTapped: @escaping (String) -> Void) {
+        self.addTapped = addTapped
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+            TextField("Item content", text: $text)
+            Button("Add") {
+                addTapped(text)
+                dismiss()
+            }
+            Spacer()
+        }
+    }
 }
