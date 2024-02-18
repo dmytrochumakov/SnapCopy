72f63bff59112b5bf97a5f7b789f6b4327d0ebcd
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-02-18 07:19:51 +0200

    added subscribeToPasteboard functinality to be able enable and disable paste button

--- Packages/Sources/Packages/AppView.swift
+++ Packages/Sources/Packages/AppView.swift
@@ -5,6 +5,8 @@
 //  Created by Dmytro Chumakov on 10.02.2024.
 //
 
+import CloudKit
+import Combine
 import SimpleToast
 import SwiftUI
 import TipKit
@@ -15,6 +17,8 @@ public struct AppView: View {
     @State private var showEditItemView: Bool = false
     @State private var showToast: Bool = false
     @State private var selectedEditItem: Item?
+    @State private var pasteButtonDisabled: Bool = UIPasteboard.general.string == nil    
+    @State private var cancellables = Set<AnyCancellable>()
 
     private let toastOptions = SimpleToastOptions(
         alignment: .bottom,
@@ -26,6 +30,7 @@ public struct AppView: View {
         try? Tips.resetDatastore()
         #endif
         try? Tips.configure()
+        subscribeToPasteboard()
     }
 
     public var body: some View {
@@ -80,12 +85,25 @@ public struct AppView: View {
             if let string = UIPasteboard.general.string {
                 context.insert(Item(name: string))
             }
-        }.buttonStyle(AppButtonStyle()).padding(16)
-            .popoverTip(GoToSettingsTip()) { _ in
-                if let url = URL(string: UIApplication.openSettingsURLString) {
-                    UIApplication.shared.open(url)
-                }
+        }
+        .disabled(pasteButtonDisabled)
+        .buttonStyle(AppButtonStyle())
+        .padding(16)
+        .popoverTip(GoToSettingsTip()) { _ in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url)
             }
+        }
+    }
+
+    private func subscribeToPasteboard() {
+        NotificationCenter
+            .default
+            .publisher(for: UIPasteboard.changedNotification)
+            .sink { _ in
+                pasteButtonDisabled = UIPasteboard.general.string == nil
+            }
+            .store(in: &cancellables)
     }
 }
 
