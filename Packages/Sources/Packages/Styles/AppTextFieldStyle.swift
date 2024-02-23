c39d37f4db4a3b5302d4e5f1ec374bba2ae744f3
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-02-23 07:27:06 +0200

    added additional trailing for text field with clear button

--- Packages/Sources/Packages/Styles/AppTextFieldStyle.swift
+++ Packages/Sources/Packages/Styles/AppTextFieldStyle.swift
@@ -12,7 +12,8 @@ struct AppTextFieldStyle: TextFieldStyle {
     func _body(configuration: TextField<_Label>) -> some View {
         configuration
             .frame(height: 44)
-            .padding(.horizontal, 16)
+            .padding(.leading, 16)
+            .padding(.trailing, 32)
             .font(.system(size: 20, weight: .regular))
             .background(Color.gray.opacity(0.1))
             .cornerRadius(10)
