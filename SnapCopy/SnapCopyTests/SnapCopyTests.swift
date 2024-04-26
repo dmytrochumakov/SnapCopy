560dadf26bd385dfea98da02287c9af039679be3
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-04-26 07:41:58 +0300

    added SnapCopyTests

---
+++ SnapCopy/SnapCopyTests/SnapCopyTests.swift
@@ -0,0 +1,16 @@
+//
+//  SnapCopyTests.swift
+//  SnapCopyTests
+//
+//  Created by Dmytro Chumakov on 26.04.2024.
+//
+
+import XCTest
+
+final class SnapCopyTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertTrue(4 == 4)
+    }
+
+}
